b36c6e8fdbcd5f50fed897e673312f5d29cc0679
Adnurhytia <adindanurhayati30@gmail.com>
2022-12-30 17:54:21 +0700

    change search

--- BinarySearchThree/Program.cs
+++ BinarySearchThree/Program.cs
@@ -35,7 +35,7 @@ namespace BinarySearchThree
         public void insert(string element) /* Insert a node in the binary search three */
         {
             Node tmp, parent = null, currentNode = null;
-            Search(element, ref parent, ref currentNode);
+            search(element, ref parent, ref currentNode);
             if (currentNode != null) /*Check if the node to be inserted already inserted or not */
             {
                 Console.WriteLine("Duplicate words not allowed");
